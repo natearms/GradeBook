4f9a7db3266574e38454de945b3bc1df01b1ccf1
Nathaniel Arms <nathaniel.arms@gmail.com>
2019-12-13 16:48:12 -0600

    Initial upload

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,99 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public delegate void GradeAddedDelegate()
+
+    public class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+        public void AddGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(100);
+                    break;
+                case 'B':
+                    AddGrade(90);
+                    break;
+                case 'C':
+                    AddGrade(80);
+                    break;
+                case 'D':
+                    AddGrade(70);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+        public void AddGrade(double grade)
+        {
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+                ValidGrade = true;
+            }
+            else
+            {
+                ValidGrade = false;
+                //throw new ArgumentException($"Invalid Grade {nameof(grade)}");
+            }
+        }
+        public Statistics GetStatistics()
+        {
+            var results = new Statistics();
+            results.Average = 0.0;
+            results.High = double.MinValue;
+            results.Low = double.MaxValue;
+
+            for (var index = 0; index < grades.Count; index++)
+            {
+                results.Low = Math.Min(grades[index], results.Low);
+                results.High = Math.Max(grades[index], results.High);
+                results.Average += grades[index];
+            }
+            
+            results.Average /= grades.Count;
+
+            switch (results.Average)
+            {
+                case var d when d >= 90.0:
+                    results.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    results.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    results.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    results.Letter = 'D';
+                    break;
+                default:
+                    results.Letter = 'F';
+                    break;
+            }
+            return results;
+        }
+
+        private List<double> grades;
+        
+        public string Name
+        {
+            get; set;
+        }
+
+
+        public const string CATEGORY = "Science";
+        public bool ValidGrade;
+        
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Nate's Grade Book");
+
+            while(true)
+            {
+                Console.WriteLine("Please enter your grades, press Q when finished");
+                var input = Console.ReadLine();
+                if(input == "Q")
+                {
+                    break;
+                }
+                
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                    
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+                
+            };
+            
+            var stats = book.GetStatistics();
+
+            Console.WriteLine(Book.CATEGORY);
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+
+        }
+    }
+}
