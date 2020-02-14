ef3a0fc587db5ce6f140a017dd3516f1b11f5273
Nathaniel Arms <nathaniel.arms@gmail.com>
2020-02-14 16:13:49 -0600

    Reset tutorial

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,56 +7,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            IBook book = new DiskBook("Nate's Grade Book");
-            book.GradeAdded += OnGradeAdded;
-
-            EnterGrades(book);
-
-            var stats = book.GetStatistics();
-
-            Console.WriteLine($"For the book named {book.Name}");
-            Console.WriteLine($"The lowest grade is {stats.Low}");
-            Console.WriteLine($"The highest grade is {stats.High}");
-            Console.WriteLine($"The average grade is {stats.Average:N1}");
-            Console.WriteLine($"The letter grade is {stats.Letter}");
-        }
-
-        private static void EnterGrades(IBook book)
-        {
-            while (true)
-            {
-                Console.WriteLine("Please enter your grades, press Q when finished");
-                var input = Console.ReadLine();
-                if (input == "Q")
-                {
-                    break;
-                }
-
-                try
-                {
-                    var grade = double.Parse(input);
-                    book.AddGrade(grade);
-
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                catch (FormatException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                finally
-                {
-                    Console.WriteLine("**");
-                }
-
-            }
-        }
-
-        static void OnGradeAdded(object sender, EventArgs e)
-        {
-            Console.WriteLine("A grade was added");
+            
         }
     }
 }
