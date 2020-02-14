f2451b5096df332b2443bbb1d4749cf34ebe78b9
Nathaniel Arms <nathaniel.arms@gmail.com>
2020-02-14 17:43:23 -0600

    .

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,7 +7,14 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
         }
     }
 }
